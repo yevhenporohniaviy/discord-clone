ae00742a071457f82d608858c4dc1f34360326e5
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2023-09-03 20:21:14 +0300

    feature/invitations

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -4,6 +4,7 @@ import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps { 
     server: ServerWithMembersWithProfiles,
@@ -13,6 +14,7 @@ interface ServerHeaderProps {
 
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+    const { onOpen } = useModal()
 
     const isAdmin = role === MemberRole.ADMIN;
     const isModerator = isAdmin || role === MemberRole.MODERATOR;
@@ -28,7 +30,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
                 {isModerator && (
-                    <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer ">
+                    <DropdownMenuItem
+                        onClick={() => onOpen('invite', { server })}
+                        className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer "
+                    >
                         Invite people
                         <UserPlus className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
